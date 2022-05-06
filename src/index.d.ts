b429688c7ac038f11dc2305cb13cec270bc6da12
Bảo Gia <baobaovabao@gmail.com>
2022-05-06 11:43:10 +0700

    edit IconName

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { ViewProps } from 'react-native';
-import { IconName as IconNameValue, IconVariant } from './value';
+import { IconName, IconVariant } from './value';
 
-export const IconName: React.ValidationMap<IconNameValue>;
+export const IconName;
 export interface IconProps extends ViewProps {
   name: IconName;
   size?: number;
