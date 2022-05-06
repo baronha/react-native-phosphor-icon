017e6a43b2f6278e04e16765df1f8facb9eabea8
Bảo Gia <baobaovabao@gmail.com>
2022-05-06 12:32:04 +0700

    edit index.tsx

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { ViewProps } from 'react-native';
-import { IconName as Name, IconVariant } from './value';
+// import * as React from 'react';
+// import { ViewProps } from 'react-native';
+// import { IconName as Name, IconVariant } from './value';
 
-export declare const IconName: React.ValidationMap<Name>;
-export interface IconProps extends ViewProps {
-  name: IconName;
-  size?: number;
-  style?: Object;
-  color?: String;
-  variant?: IconVariant;
-}
+// export declare const IconName: React.ValidationMap<Name>;
+// export interface IconProps extends ViewProps {
+//   name: IconName;
+//   size?: number;
+//   style?: Object;
+//   color?: String;
+//   variant?: IconVariant;
+// }
 
-declare const Icon: React.FunctionComponent<IconProps>;
+// declare const Icon: React.FunctionComponent<IconProps>;
 
-export default Icon;
+// export default Icon;
